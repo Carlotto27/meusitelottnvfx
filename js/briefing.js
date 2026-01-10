d90636fa9c641a3b40940767e5303184115bcfbd
Carlotto27 <carlose.carlotto@gmail.com>
2026-01-10 09:13:50 -0300

    Update briefing.js

--- js/briefing.js
+++ js/briefing.js
@@ -19,6 +19,51 @@ document.getElementById("briefingForm").addEventListener("submit", function(e){
     }
   );
 });
+(function () {
+  emailjs.init("pydNQWda7pLebHu5k");
+})();
+
+const form = document.getElementById("briefingForm");
+const submitBtn = form.querySelector(".btn-submit");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // Evita duplo envio
+  if (submitBtn.disabled) return;
+
+  // Bloqueia botão
+  submitBtn.disabled = true;
+  submitBtn.innerText = "Enviando...";
+  submitBtn.style.opacity = "0.7";
+  submitBtn.style.cursor = "not-allowed";
+
+  emailjs.sendForm(
+    "service_usdk0lc",
+    "template_3ozcieb",
+    this
+  ).then(
+    function () {
+      submitBtn.innerText = "Enviado com sucesso ✔️";
+
+      // Opcional: limpar formulário
+      form.reset();
+
+    },
+    function (error) {
+      console.log(error);
+
+      // Reativa botão em caso de erro
+      submitBtn.disabled = false;
+      submitBtn.innerText = "Enviar briefing";
+      submitBtn.style.opacity = "1";
+      submitBtn.style.cursor = "pointer";
+
+      alert("Erro ao enviar. Tente novamente.");
+    }
+  );
+});
+
 
 const inputs = document.querySelectorAll(
   'input[type="text"], textarea'
