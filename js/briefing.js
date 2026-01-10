bec1aa3bea990842be15a9f4c82171bd6fe14087
Carlotto27 <carlose.carlotto@gmail.com>
2026-01-10 01:44:38 -0300

    BRIEFING2

--- js/briefing.js
+++ js/briefing.js
@@ -19,3 +19,27 @@ document.getElementById("briefingForm").addEventListener("submit", function(e){
     }
   );
 });
+
+const inputs = document.querySelectorAll(
+  'input[type="text"], textarea'
+);
+
+inputs.forEach(el => {
+  el.addEventListener('focus', () => {
+
+    const rect = el.getBoundingClientRect();
+    const isVisible =
+      rect.top > window.innerHeight * 0.25 &&
+      rect.bottom < window.innerHeight * 0.75;
+
+    if (!isVisible) {
+      setTimeout(() => {
+        el.scrollIntoView({
+          behavior: 'smooth',
+          block: 'center'
+        });
+      }, 300);
+    }
+
+  });
+});
